697ef8e45824970173291a291194014f13322f46
kevin1121-bit <gutierrez01kevin@gmail.com>
2020-12-03 15:26:37 -0500

    kevin

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './assets/logo.svg';
+import Header from "./components/header";
+import Dish from "./components/dish";
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <Header />
+          <Dish />
+      </div>
+     
+  );
+}
+
+export default App;
---
+++ src/components/dish.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Dish extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Barberos</h1>
+            </div>
+        );
+    }
+}
+export default Dish;
